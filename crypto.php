b488389b35b8743523d86b29da925b782f4aaa84
Abu Ayyub Al Ansháry <luthfie@y7mail.com>
2016-05-23 09:05:03 +0700

    Update crypto.php

--- crypto.php
+++ crypto.php
@@ -8,6 +8,10 @@
  * github.com/9r3i
  * 
  * ~ version 1.0 - september 21st 2015
+ * ~ version 1.1 - may 23rd 2016
+ *   + change parameter $i, to string of openssl method
+ *     the reason is the different of php version
+ *   + fix forgotten catch in try and throw exception
  * 
  * Usage:
  * $cr = new crypto($method,$encode);
@@ -23,28 +27,31 @@
  *     but i prefer OPENSSL_RAW_DATA in this case 
  */
 class crypto{
-  public $version='1.0';
+  public $version='1.1';
   public $methods;
   private $method;
   private $encode;
-  function __construct($i=0,$e=false){
+  function __construct($i=null,$e=false){
     $this->methods=openssl_get_cipher_methods();
-    $this->method=isset($i,$this->methods[$i])?$this->methods[$i]:$this>methods[0];
-    $this->encode=isset($e)&&$e?true:false;
+    $this->method=isset($i)&&in_array($i,$this->methods)?$i:'AES-128-CBC';
+    $this->encode=is_bool($e)&&$e?true:false;
   }
-  public function encrypt($s,$c){
+  public function encrypt($s=null,$c=null){
     $n=openssl_cipher_iv_length($this->method);
     $o=openssl_random_pseudo_bytes($n);
-    $t=openssl_encrypt($s,$this->method,$c,OPENSSL_RAW_DATA,$o);
+    $t=openssl_encrypt((string)$s,$this->method,$c,OPENSSL_RAW_DATA,$o);
     return $this->encode?base64_encode($o.$t):$o.$t;
   }
-  public function decrypt($s,$c){
-    if($this->encode){$s=base64_decode($s,true);
-      if($s===false){throw new Exception('Decryption failure');}
-    }
+  public function decrypt($s=null,$c=null){
+    if($this->encode){try{
+      $s=base64_decode((string)$s,true);
+      if($s===false){throw new \Exception('Decryption failure');}
+    }catch(\Exception $e){
+      return false;
+    }}
     $n=openssl_cipher_iv_length($this->method);
-    $o=mb_substr($s,0,$n,'8bit');
-    $t=mb_substr($s,$n,null,'8bit');
+    $o=mb_substr((string)$s,0,$n,'8bit');
+    $t=mb_substr((string)$s,$n,null,'8bit');
     return openssl_decrypt($t,$this->method,$c,OPENSSL_RAW_DATA,$o);
   }
 }
